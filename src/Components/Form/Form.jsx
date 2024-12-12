e211d12e98da7a70826a1197ff34d0163361264d
Marius R <36150783+Marsol0@users.noreply.github.com>
2024-12-12 19:32:59 +0200

    adding array of notes

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import styles from "./Form.module.css"
 
-const Form = () => {
+const Form = (props) => {
     const [note, setNote] = useState({
         title: "",
         content: ""
@@ -17,14 +17,15 @@ const Form = () => {
        }))
     }
     const handleClick = (e) => {
+        props.addNote(note)
         e.preventDefault()
         
     }
     return (
         <form>
             <input type="text" placeholder="Title" name="title" value={note.title} onChange={onChangeFn}/>
-            <textarea rows={6} placeholder="What's on you're mind?" name="content" value={note.content} onChange={onChangeFn}></textarea>
-            <button onClick={handleClick}>add</button>
+            <textarea rows={6} placeholder="What's on your mind?" name="content" value={note.content} onChange={onChangeFn}></textarea>
+            <button onClick={handleClick}>Add</button>
         </form>
     )
 }
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
 import react from "react"
 import Form from "../Form/Form"
+import styles from "./Main.module.css"
 const Main = () => {
+    const [allNotes, setAllNotes] = useState([])
     return (
+        <>
         <Form />
+        <div className={styles.noteConteiner}>
+            <div className={styles.noteList}>
+                <h3>asd</h3>
+                <p>asdasdasdasdad</p>
+            </div>
+           
+        </div>
+        </>
     )
 }
 
