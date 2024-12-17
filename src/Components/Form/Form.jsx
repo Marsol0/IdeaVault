7a79d5818e705bfc20002ad0d5a0ac588e55d66d
Marius R <36150783+Marsol0@users.noreply.github.com>
2024-12-17 16:02:22 +0200

    adding extending textarea and styling

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,40 +1,58 @@
 import React, { useState } from "react";
 
-import styles from "./Form.module.css"
+import styles from "./Form.module.css";
 
 const Form = (props) => {
-    const [note, setNote] = useState({
-        title: "",
-        content: ""
-    })
-    
-    const onChangeFn = (e) => { 
-        const { name, value } = e.target
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  const [shrinkRows, setShtinkRows] = useState(false);
 
-       setNote((prevValue) => ({
-        ...prevValue,
-        [name]: value
-       }))
-    }
-    const handleClick = (e) => {
-        props.onAdd(note)
-        setNote({
-            title: "",
-            content: ""
-        })
-        e.preventDefault()
-        
-        
-        
-        
-    }
-    return (
-        <form>
-            <input type="text" placeholder="Title" name="title" value={note.title} onChange={onChangeFn}/>
-            <textarea rows={6} placeholder="What's on your mind?" name="content" value={note.content} onChange={onChangeFn}></textarea>
-            <button onClick={handleClick}>Add</button>
-        </form>
-    )
-}
+  const onChangeFn = (e) => {
+    const { name, value } = e.target;
 
-export default Form
+    setNote((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+  };
+  const handleClick = (e) => {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    e.preventDefault();
+  };
+
+  const handleExpand = () => {
+    setShtinkRows(true);
+    console.log(shrinkRows);
+  };
+  return (
+    <form>
+        {shrinkRows && (
+            <input
+            type="text"
+            placeholder="Title"
+            name="title"
+            value={note.title}
+            onChange={onChangeFn}
+          />
+        )}
+      
+      <textarea
+        onClick={handleExpand}
+        rows={shrinkRows ? 1 : 1}
+        placeholder="What's on your mind?"
+        name="content"
+        value={note.content}
+        onChange={onChangeFn}
+      ></textarea>
+      {shrinkRows && <button className={styles.submitNote}onClick={handleClick}>+</button>}
+    </form>
+  );
+};
+
+export default Form;
