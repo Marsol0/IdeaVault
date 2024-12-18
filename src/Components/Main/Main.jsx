e807894bdc203afe8be4c237a5fde09bc5cc08d2
Marius R <36150783+Marsol0@users.noreply.github.com>
2024-12-18 20:44:22 +0200

    adding creation time, adding new colors

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -8,6 +8,17 @@ const Main = () => {
   const [allNotes, setAllNotes] = useState([]);
   const [error, setError] = useState(null);
 
+  const formatDate = (date) =>
+    new Date(date)
+      .toLocaleString("en-US", {
+        month: "short",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+      .replace(",", "");
+
   useEffect(() => {
     axios
       .get("http://localhost:3002/api/notes")
@@ -20,38 +31,49 @@ const Main = () => {
       setError("Please fill in both the title and content fields.");
       return;
     }
-
-    axios
+    try {
+      axios
       .post("http://localhost:3002/api/notes", newNote)
       .then((response) => {
         setAllNotes((prev) => [...prev, response.data]);
         setError(null);
-        console.log(allNotes)
+        console.log(allNotes);
       })
-      .catch(() => setError("Failed to add note."));
+      
+    } catch {
+      setError("Failed to add new note. Please try again")
+    }
+    
   };
 
-  const deleteNote = (id) => {
-    axios
-      .delete(`http://localhost:3002/api/notes/${id}`)
-      .then(() => setAllNotes((prev) => prev.filter((note) => note.id !== id)))
-      .catch(() => setError("Failed to delete note."));
+  const deleteNote = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3002/api/notes/${id}`);
+      setAllNotes((prev) => prev.filter((note) => note.id !== id));
+    } catch {
+      setError("Failed to delete note. Please try again.");
+    }
   };
+  
 
   return (
     <>
-    <Form onAdd={addNote} />
-    {error && <h3 className={styles.error}>{error}</h3>}
-    <div className={styles.noteConteiner}>
-      {allNotes.map((note) => (
-        <div key={note.id} className={styles.note}>
-          <h3>{note.note_data.title}</h3> 
-          <p>{note.note_data.content}</p> 
-          <button onClick={() => deleteNote(note.id)}>Delete</button>
-        </div>
-      ))}
-    </div>
-  </>
+      <Form onAdd={addNote} />
+
+      {error && <h3 className={styles.error}>{error}</h3>}
+      <div className={styles.noteConteiner}>
+        {allNotes.map((note) => (
+          <div key={note.id} className={styles.note}>
+            <span>
+              {formatDate(note.created_at)}
+            </span>
+            <h3>{note.note_data.title}</h3>
+            <p>{note.note_data.content}</p>
+            <button onClick={() => deleteNote(note.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
